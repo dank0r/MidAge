c9ae898ac7be0a6b71d0c43aee31e9314df78c7b
H4CKiT <h4ckit@mail.ru>
2019-09-19 21:29:49 +0300

    Options for tilemap

---
+++ Assets/Scenes/Starter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class Starter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {	
+		Tilemap tilemap = GetComponent<Tilemap>();
+
+		BoundsInt bounds = tilemap.cellBounds;
+		TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+		TileBase tile = allTiles [0];
+		Debug.Log (allTiles.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
