d115831e538fb124e69498a6584503c7a5c14e92
H4CKiT <h4ckit@mail.ru>
2019-09-24 01:32:47 +0300

    Better jump

---
+++ Assets/Jump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+	//Jumping
+	[Range(1, 10)]
+	public float jumpVelocity;
+	private bool isGrounded;  
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		isGrounded = true;
+	}
+
+	// Use this for initialization
+	void Start () {
+		isGrounded = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Jump
+		if (Input.GetButton ("Jump") && isGrounded) {
+			GetComponent<Rigidbody2D> ().velocity = Vector2.up * jumpVelocity;
+			isGrounded = false;
+		}
+	}
+}
--- Assets/PlayerControleer.cs
+++ Assets/PlayerControleer.cs
@@ -1,26 +1,26 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerControleer : MonoBehaviour {
 
+	//movement AxisX
 	public float speed = 20f;
+
+
+	//GetComponent
 	private Rigidbody2D rb;
 
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
-
 	}
 
 	void Update () {
 		float moveX = Input.GetAxis ("Horizontal"); 
-		if (moveX > 0) {
+		if (moveX > 0) 
 			GetComponent<SpriteRenderer> ().flipX = false;
-		} else {
+		else if (moveX < 0)
 			GetComponent<SpriteRenderer> ().flipX = true;
-			
-		}
-		rb.MovePosition (rb.position + Vector2.right * moveX * speed * Time.deltaTime);
-
-		if (Input.GetKeyDown (KeyCode.Space))
-			rb.AddForce (Vector2.up * 80000 * Time.deltaTime);
+		
+		//rb.MovePosition (rb.position + Vector2.right * moveX * speed * Time.deltaTime);
 	}
 }
