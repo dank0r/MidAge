8effe6663e7b5070d5a2b71aecb637f5c5b3fdb5
H4CKiT <h4ckit@mail.ru>
2019-09-24 03:51:03 +0300

    Add sword add bugged animation

---
+++ Assets/Attack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+
+	private Animator an;
+
+	// Use this for initialization
+	void Start () {
+		an = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Jump")) {
+			an.SetBool ("isAttack", true);
+		} else {
+			an.SetBool ("isAttack", false);
+		}
+	}
+}
