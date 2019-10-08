92031f4e089a09455197786fa695a2212a66bebf
dank0r <dan296dan@gmail.com>
2019-10-08 18:50:51 +0300

    fix some typos

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+	//movement AxisX
+	public float speed = 20f;
+	public float shiftSpeed = 2f;
+	private float curSpeed;
+
+	//GetComponent
+	private Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		curSpeed = speed;
+	}
+
+	void Update () {
+
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			curSpeed = shiftSpeed;
+		} else {
+			curSpeed = speed;
+		}
+
+		float moveX = Input.GetAxis ("Horizontal"); 
+		rb.velocity = new Vector2(moveX * curSpeed, rb.velocity.y);
+
+
+
+
+		if (moveX > 0) {
+			GetComponent<SpriteRenderer> ().flipX = false;
+		} else if (moveX < 0) {
+			GetComponent<SpriteRenderer> ().flipX = true;
+		}
+	}
+}
---
+++ Assets/SwordController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordController : MonoBehaviour {
+
+	public GameObject character;
+	private Vector2 posOfCharacter;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 sw = GetComponent<Transform> ().position;
+		Vector3 ch = character.GetComponent<Transform> ().position;
+
+		posOfCharacter = new Vector2(sw.x, sw.y) - new Vector2(ch.x, ch.y);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 ch = character.GetComponent<Transform> ().position;
+		Vector2 pos = new Vector2 (ch.x, ch.y) + posOfCharacter;
+		GetComponent<Transform> ().position = new Vector3(pos.x, pos.y, 0);
+	}
+}
